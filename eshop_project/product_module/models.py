fdc331eace7a5f18f11e029b80fd3b8e5fcd869f
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-06-28 18:53:21 +0200

    Created Product Model

--- eshop_project/product_module/models.py
+++ eshop_project/product_module/models.py
@@ -1,4 +1,6 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+
 
 # Create your models here.
 
@@ -6,3 +8,9 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=300)
     price = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=0)
+    short_description = models.CharField(max_length=360, null=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.price})"
