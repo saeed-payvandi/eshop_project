8cfa080f5170f8be47968188c653d5966ac7cb79
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-06-29 20:39:20 +0200

    Added product_list and product_detail templates to the project

--- eshop_project/product_module/views.py
+++ eshop_project/product_module/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'product_module/product_list.html', context)
