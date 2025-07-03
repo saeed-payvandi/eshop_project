9342ee506023f66c2d70d3ab6942b21da90da281
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-07-03 21:55:01 +0200

    Added count of products and avg_rating to product_list template

--- eshop_project/product_module/views.py
+++ eshop_project/product_module/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.http import Http404
+from django.db.models import Avg, Min, Max
 
 
 # Create your views here.
 
 def product_list(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-title')
+    number_of_products = products.count()
+    avg_rating = products.aggregate(Avg("rating"), Avg("price"), Min("price"), Max("price"))
     context = {
         'products': products,
+        'total_number_of_products': number_of_products,
+        'average_ratings': avg_rating,
     }
 
     return render(request, 'product_module/product_list.html', context)
