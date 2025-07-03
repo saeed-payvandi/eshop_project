76b151e7a3826e369166f8277c62fce94efb1ef7
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-07-03 12:14:19 +0200

    Add slug field and override save method of Product model

--- eshop_project/product_module/models.py
+++ eshop_project/product_module/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -12,9 +13,14 @@ class Product(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=0)
     short_description = models.CharField(max_length=360, null=True)
     is_active = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     def get_absolute_url(self):
         return reverse('product-detail', args=[self.id])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.price})"
