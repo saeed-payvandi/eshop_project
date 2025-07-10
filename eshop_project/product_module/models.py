9125c77984bc318d64be2a712ade4c0e6d9b342c
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-07-10 17:43:50 +0200

    Modified category field of Product model

--- eshop_project/product_module/models.py
+++ eshop_project/product_module/models.py
@@ -16,7 +16,7 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=300)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True, related_name='products')
     price = models.IntegerField()
     rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], default=0)
     short_description = models.CharField(max_length=360, null=True)
