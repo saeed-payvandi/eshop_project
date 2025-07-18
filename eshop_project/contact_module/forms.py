50314458627bb16d39d596718f2fa7efd17a3655
saeed payvandi <saeedpayvandi82@gmail.com>
2025-07-18 17:50:41 +0100

    Modified ContactUsForms and contact_us_page

--- eshop_project/contact_module/forms.py
+++ eshop_project/contact_module/forms.py
@@ -8,10 +8,32 @@ class ContactUsForms(forms.Form):
         # required=False,
         error_messages={
             'required': 'لطفا نام و نام خانوادگی خود را وارد کنید',
-        }
-        )
-    email = forms.EmailField(label='ایمیل', widget=forms.EmailInput)
-    subject = forms.CharField(label='عنوان')
-    text = forms.CharField(label='متن پیام', widget=forms.Textarea)
-    # text = forms.CharField(widget=forms.Textarea)
-
+            'max_length': 'نام و نام خانوادگی نمی تواند بیشتر از 50 کاراکتر باشد'
+        },
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'نام و نام خانوادگی',
+        }))
+    email = forms.EmailField(
+        label='ایمیل', 
+        # required=False,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'ایمیل',
+        }))
+    subject = forms.CharField(
+        label='عنوان',
+        # required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'عنوان',
+        }))
+    text = forms.CharField(
+        label='متن پیام', 
+        # required=False,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'id': 'message',
+            'rows': '5',
+            'placeholder': 'متن پیام',
+        }))
