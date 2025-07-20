702436c8a9ff4497deb703113dad314004169ff1
saeed payvandi <saeedpayvandi82@gmail.com>
2025-07-21 00:42:37 +0100

    Used ListView for ProductListView class

--- eshop_project/home_module/urls.py
+++ eshop_project/home_module/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    # path('', TemplateView.as_view(template_name = 'home_module/index_page.html'))
     path('', views.HomeView.as_view(), name='home_page'),
     # path('', views.index_page, name='home_page'),
     # path('site-header', views.site_header_component, name='site_header_component'),
--- eshop_project/product_module/views.py
+++ eshop_project/product_module/views.py
@@ -2,9 +2,48 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, ProductCategory
 from django.http import Http404
 from django.db.models import Avg, Min, Max
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+
 
 
 # Create your views here.
+
+
+class ProductListView(ListView):
+    template_name = 'product_module/product_list.html'
+    model = Product
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        # base_query = super(ProductListView, self).get_queryset()
+        base_query = super().get_queryset()
+        data = base_query.filter(is_active=True)
+        return data
+
+
+# class ProductListView(TemplateView):
+#     template_name = 'product_module/product_list.html'
+
+#     def get_context_data(self, **kwargs):
+#         product = Product.objects.all().order_by('price')[:5]
+#         context = super().get_context_data(**kwargs)
+#         # context = super(ProductListView, self).get_context_data()
+#         context['products'] = product
+#         return context
+    
+
+class ProductDetailView(TemplateView):
+    template_name = 'product_module/product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = kwargs['slug']
+        product = get_object_or_404(Product, slug=slug)
+        context['product'] = product
+        return context
+
+
 
 def product_list(request):
     # console = ProductCategory(title='پلی استیشن', url_title='playstation')
