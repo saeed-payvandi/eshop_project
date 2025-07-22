8247890b546cea60283f4fd9d2137ee258c6560b
saeed payvandi <saeedpayvandi82@gmail.com>
2025-07-23 00:33:58 +0100

    Modified UserProfile model and updated .gitignore

--- eshop_project/contact_module/models.py
+++ eshop_project/contact_module/models.py
@@ -22,4 +22,4 @@ class ContactUs(models.Model):
 
 class UserProfile(models.Model):
     # image = models.FileField(upload_to='images')
-    image = models.ImageField(upload_to='uploads/images')
+    image = models.ImageField(upload_to='images')
