326fda0200556af2f0979f2ec589eb3fe47859ab
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-07-27 20:07:37 +0200

    Creared account_module app and own User model and refactor the database

---
+++ eshop_project/account_module/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
+from django.db import models
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    mobile = models.CharField(max_length=20, verbose_name='تلفن همراه')
+    email_active_code = models.CharField(max_length=100, verbose_name='کد فعالسازی ایمیل')
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+
+    def __str__(self):
+        return self.get_full_name()
+
+
+# class UserProfile(models.Model):
+#     pass
--- eshop_project/eshop_project/settings.py
+++ eshop_project/eshop_project/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Internal apps:
+    'account_module',
     'product_module',
     'home_module',
     'contact_module',
+    # External apps:
     'django_render_partial',
 ]
 
@@ -77,6 +80,8 @@ WSGI_APPLICATION = 'eshop_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+AUTH_USER_MODEL = 'account_module.User'
 
 DATABASES = {
     'default': {
