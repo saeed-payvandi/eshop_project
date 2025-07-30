c3d0a55cef61d2612e3291ccddb19180d2d62ee5
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-07-30 20:15:17 +0200

    continued registration task

--- eshop_project/account_module/views.py
+++ eshop_project/account_module/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import RegisterForm
+from .models import User
+from django.utils.crypto import get_random_string
 # from django.contrib.auth import get_user_model
 
 # Create your views here.
@@ -18,11 +20,20 @@ class RegisterView(View):
 
     def post(self, request):
         register_form = RegisterForm(request.POST)
-
         if register_form.is_valid():
-            print(register_form.cleaned_data)
-            # todo: register user
-            pass
+            user_email = register_form.cleaned_data.get('email')
+            user_password = register_form.cleaned_data.get('password')
+            user: bool = User.objects.filter(email__exact=user_email).exists()
+            if user:
+                register_form.add_error('email', 'ایمیل وارد شده تکراری می باشد')
+            else:
+                new_user = User(email=user_email, email_active_code=get_random_string(72))
+                new_user.set_password(user_password)
+                new_user.save()
+                # todo: send email active code
+
+
+
 
         context = {
             'register_form': register_form,
