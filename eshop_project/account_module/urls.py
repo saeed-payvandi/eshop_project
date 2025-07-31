e2e67479beb891e7ff211f780aa8c53df557429e
saeed payvandi <saeedpayvandi82@gmail.com>
2025-08-01 00:25:57 +0100

    continued registration task and created LoginView

--- eshop_project/account_module/urls.py
+++ eshop_project/account_module/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register_page'),
+    path('login', views.LoginView.as_view(), name='login_page'),
 ]
--- eshop_project/account_module/views.py
+++ eshop_project/account_module/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import RegisterForm
 from .models import User
 from django.utils.crypto import get_random_string
+from django.urls import reverse
 # from django.contrib.auth import get_user_model
 
 # Create your views here.
@@ -23,14 +24,19 @@ class RegisterView(View):
         if register_form.is_valid():
             user_email = register_form.cleaned_data.get('email')
             user_password = register_form.cleaned_data.get('password')
-            user: bool = User.objects.filter(email__exact=user_email).exists()
+            user: bool = User.objects.filter(email__iexact=user_email).exists()
             if user:
                 register_form.add_error('email', 'ایمیل وارد شده تکراری می باشد')
             else:
-                new_user = User(email=user_email, email_active_code=get_random_string(72))
+                new_user = User(
+                    email=user_email, 
+                    username=user_email, 
+                    is_active=False, 
+                    email_active_code=get_random_string(72))
                 new_user.set_password(user_password)
                 new_user.save()
                 # todo: send email active code
+                return redirect(reverse('login_page'))
 
 
 
@@ -39,4 +45,15 @@ class RegisterView(View):
             'register_form': register_form,
         }
         return render(request, 'account_module/register.html', context)
+    
+
+class LoginView(View):
+    def get(self, request):
+        context = {
+            'login_form': None,
+        }
+        return render(request,'account_module/register.html', context)
+
+    def post(self, request):
+        pass
 
