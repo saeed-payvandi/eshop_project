cf9c7de3ac8990788625204f4a99b3db3fae94da
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-10-21 14:58:54 +0200

    Implement add_product_to_order view

--- eshop_project/eshop_project/urls.py
+++ eshop_project/eshop_project/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('products/', include('product_module.urls')),
     path('articles/', include('article_module.urls')),
     path('user/', include('user_panel_module.urls')),
+    path('order/', include('order_module.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- eshop_project/order_module/views.py
+++ eshop_project/order_module/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from product_module.models import Product
 
 # Create your views here.
+
+
+def add_product_to_order(request: HttpRequest):
+    product_id = request.GET.get('product_id')
+    count = request.GET.get('count')
+
+    if request.user.is_authenticated:
+        product = Product.objects.filter(pk=product_id, is_active=True, is_delete=False).first()
+        if product is not None:
+            pass
+    else:
+        return JsonResponse({
+            'status': 'not_auth'
+        })
+    
