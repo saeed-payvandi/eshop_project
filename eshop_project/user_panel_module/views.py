284f3d85c54543e18ff9752e694c2fd69265d42a
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-10-27 14:13:08 +0100

    Use login_required decorator for user_panel views

--- eshop_project/user_panel_module/views.py
+++ eshop_project/user_panel_module/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpRequest, JsonResponse
@@ -13,10 +14,12 @@ from .forms import EditProfileModelForm, ChangePasswordForm
 # Create your views here.
 
 
+@method_decorator(login_required, name='dispatch')
 class UserPanelDashboardPage(TemplateView):
     template_name = 'User_panel_module/user_panel_dashboard_page.html'
 
 
+@method_decorator(login_required, name='dispatch')
 class EditUserProfilePage(View):
     def get(self, request: HttpRequest):
         current_user = User.objects.filter(id=request.user.id).first()
@@ -44,6 +47,7 @@ class EditUserProfilePage(View):
         return render(request, 'user_panel_module/edit_profile_page.html', context)
 
 
+@method_decorator(login_required, name='dispatch')
 class ChangePasswordPage(View):
     def get(self, request: HttpRequest):
         context = {
@@ -69,11 +73,12 @@ class ChangePasswordPage(View):
         return render(request, 'user_panel_module/change_password_page.html', context)
 
 
-# @login_required
+@login_required
 def user_panel_menu_component(request: HttpRequest):
     return render(request, 'user_panel_module/components/user_panel_menu_component.html')
 
 
+@login_required
 def user_basket(request: HttpRequest):
     current_order, created = Order.objects.prefetch_related('orderdetail_set').get_or_create(is_paid=False, user_id=request.user.id)
     total_amount = current_order.calculate_total_price()
@@ -85,6 +90,7 @@ def user_basket(request: HttpRequest):
     return render(request, 'user_panel_module/user_basket.html', context)
 
 
+@login_required
 def remove_order_detail(request: HttpRequest):
     detail_id = request.GET.get('detail_id')
     if detail_id is None:
@@ -114,6 +120,7 @@ def remove_order_detail(request: HttpRequest):
     })
 
 
+@login_required
 def change_order_detail_count(request: HttpRequest):
     detail_id = request.GET.get('detail_id')
     state = request.GET.get('state')
