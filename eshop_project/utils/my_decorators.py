d1da8e24f9c46ce2ccaa3209a45a154ab50bd82d
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-11-04 18:41:23 +0100

    Implement permission_checker_decorator_factory for admin-panel

---
+++ eshop_project/utils/my_decorators.py
@@ -0,0 +1,27 @@
+from django.http import HttpRequest
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+def permission_checker_decorator_factory(data=None):
+    # print(data)
+    def permission_checker_decorator(func):
+        # print(data)
+        def wrapper(request: HttpRequest, *args, **kwargs):
+            print(data)
+            if request.user.is_authenticated and request.user.is_superuser:
+                return func(request, *args, **kwargs)
+            else:
+                return redirect(reverse('login_page'))
+        return wrapper
+    return permission_checker_decorator
+
+
+def permission_checker_dec(func):
+    def wrapper(request: HttpRequest, *args, **kwargs):
+        if request.user.is_authenticated and request.user.is_superuser:
+            return func(request, *args, **kwargs)
+        else:
+            return redirect(reverse('login_page'))
+
+    return wrapper
